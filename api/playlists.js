f0cb7138e9b47e92fbd2151d7f322cc17db6b944
Joy Harrington <joysyk23@gmail.com>
2024-10-16 14:54:20 -0500

    http and routers

--- api/playlists.js
+++ api/playlists.js
@@ -1,3 +1,50 @@
 const express = require("express");
 const router = express.Router();
 module.exports = router;
+
+const prisma = require("../prisma");
+const { authenticate } = require("./auth");
+
+router.get("/", authenticate, async (req, res, next) => {
+  try {
+    const playlists = await prisma.playlist.findMany();
+    res.json(playlists);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/", authenticate, async (req, res, next) => {
+  const { name, description, ownerId, trackIds } = req.body;
+  const tracks = trackIds.map((id) => ({ id: +id }));
+  try {
+    const playlist = await prisma.playlist.create({
+      data: {
+        name,
+        description,
+        ownerId: +ownerId,
+        tracks: { connect: tracks },
+      },
+      include: {
+        owner: true,
+        tracks: true,
+      },
+    });
+    res.json(playlist);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", authenticate, async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const playlist = await prisma.playlist.findUniqueOrThrow({
+      where: { id: +id },
+      include: { tracks: true },
+    });
+    res.json(playlist);
+  } catch (e) {
+    next(e);
+  }
+});
---
+++ api/tracks.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+module.exports = router;
+
+const prisma = require("../prisma");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const tracks = await prisma.track.findMany();
+    res.json(tracks);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const track = await prisma.track.findUniqueOrThrow({
+      where: { id: +id },
+      include: { playlists: true },
+    });
+    res.json(track);
+  } catch (e) {
+    next(e);
+  }
+});
