eff614536b77a3a5446ff281e6e7343595646ca2
Joy Harrington <joysyk23@gmail.com>
2024-10-16 14:23:10 -0500

    Created Auth Router

---
+++ api/auth.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const router = express.Router();
+
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+
+function createToken(id) {
+  return jwt.sign({ id }, JWT_SECRET, { expiresIn: "1d" });
+}
+
+const prisma = require("../prisma");
+
+// This token-checking middleware should run before any other routes.
+// It's the first in this file, and this router is imported first in `server.js`.
+router.use(async (req, res, next) => {
+  // Grab token from headers only if it exists
+  const authHeader = req.headers.authorization;
+  const token = authHeader?.slice(7); // "Bearer <token>"
+  if (!token) return next();
+  try {
+    const { id } = jwt.verify(token, JWT_SECRET);
+    const user = await prisma.user.findUniqueOrThrow({
+      where: { id },
+    });
+    req.user = user;
+    next();
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/register", async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const user = await prisma.user.register(username, password);
+    const token = createToken(user.id);
+    res.status(201).json({ token });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const user = await prisma.user.login(username, password);
+    const token = createToken(user.id);
+    res.json({ token });
+  } catch (e) {
+    next(e);
+  }
+});
+
+/** Checks the request for an authenticated user. */
+function authenticate(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    next({ status: 401, message: "You must be logged in." });
+  }
+}
+
+// Notice how we export the router _and_ the `authenticate` middleware!
+module.exports = {
+  router,
+  authenticate,
+};
